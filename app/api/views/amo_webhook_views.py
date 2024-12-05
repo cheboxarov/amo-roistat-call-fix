5fb60305d026e8053e1ccdf804149b7f9d177273
wlove <lalakasuper2@gmail.com>
2024-12-05 16:25:25 +0700

    Решил проблему

--- app/api/views/amo_webhook_views.py
+++ app/api/views/amo_webhook_views.py
@@ -29,13 +29,15 @@ class AmoWebhookView(APIView):
             api = amo_project.get_api()
             lead = api.leads.get_by_id(int(lead_id))
             logger.debug(f"\nНайден лид - {lead}\nnote_id = {note_id}\ncreated_by = {created_by}\n")
-            if not int(lead.created_at / 100) == int(time.time() / 100):
-                logger.debug(f"Слишком давно создан")
-                return Response({"status": "ok"}, status=200)
+            # if not int(lead.created_at / 100) == int(time.time() / 100):
+            #     logger.debug(f"Слишком давно создан")
+            #     return Response({"status": "ok"}, status=200)
+
+            logger.debug(f"Сделка создана: {lead.created_at} Сейчас {time.time()} Разница во времени {int(lead.created_at / 100) - int(time.time() / 100)}")
             
             if int(note_id) == 10 and not LeadProcessed.objects.filter(lead_id=lead_id).exists():
                 lead.responsible_user_id = created_by
-                new_lead = api.leads.update(lead)
+                api.leads.update(lead)
                 LeadProcessed.objects.create(lead_id=lead_id)
         except Exception as error:
             logger.error(f"error for get lead - {error}")
