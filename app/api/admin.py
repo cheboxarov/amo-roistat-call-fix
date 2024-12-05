4d997b9ca0bb0d6a292c994c58c28c7993627019
wlove <lalakasuper2@gmail.com>
2024-12-05 14:32:42 +0700

    Добавил LeadProcessed в админку

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import AmoProject, AmoWidget
+from .models import AmoProject, AmoWidget, LeadProcessed
 
 
 admin.site.register(AmoWidget)
 admin.site.register(AmoProject)
+admin.site.register(LeadProcessed)
